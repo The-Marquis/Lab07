45bc5cf4bdb202735a34be7f52c5954fc80643ac
garrettbadger <garrett.badger.id@gmail.com>
2023-06-16 15:26:16 -0600

    Velocity tests

--- testVelocity.h
+++ testVelocity.h
@@ -1,1 +1,204 @@
 #pragma once
+/***********************************************************************
+ * Header File:
+ *    Test Velocity : Test the Velocity class
+ * Author:
+ *    Br. Helfrich
+ * Summary:
+ *    All the unit tests for the Velocity class
+ ************************************************************************/
+
+
+#pragma once
+
+#include "velocity.h"
+#include <cassert>
+#include <vector>
+
+
+using namespace std;
+
+/*******************************
+ * TEST Velocity
+ * A friend class for Velocity which contains the Velocity unit tests
+ ********************************/
+class TestVelocity
+{
+public:
+    void run()
+    {
+        constructor();
+
+        setDX();
+        setDXDY();
+
+        setDY();
+
+        addDX();
+        addDY();
+        addDX2();
+        addDY2();
+
+        setSpeed();
+
+        reverse();
+        reverse2();
+    }
+
+private:
+
+    // Test the default constructor
+    void constructor()
+    {  // setup
+        Velocity v;
+        // exercise
+        double dx = v.getDX();
+        double dy = v.getDY();
+
+        // verify
+        assert(dx == 0);
+        assert(dy == 0);
+        // teardown
+
+    }
+
+
+    void setDX()
+    {  // setup
+        Velocity v;
+        // exercise
+        v.setDX(5.4);
+
+        // verify
+        assert(v.getDX() == 5.4);
+        
+        // teardown
+
+    }
+    void setDY()
+    {  // setup
+        Velocity v;
+        // exercise
+        
+        v.setDY(8.3);
+
+        // verify
+        assert(v.getDY() == 8.3);
+        
+        // teardown
+
+    }
+    void setDXDY()
+    {
+        // setup
+        Velocity v;
+        // exercise
+
+        v.setDxDy(8.3, 5.4);
+
+        // verify
+        assert(v.getDX() == 8.3);
+        assert(v.getDY() == 5.4);
+
+        // teardown
+    }
+
+    void setSpeed()
+    {
+        // setup
+        Velocity v;
+        // exercise
+
+        v.setSpeed(8.3);
+
+        // verify
+        assert(v.getSpeed() == 8.3);
+      
+
+        // teardown
+    }
+    void addDX()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDX(8.3);
+        v.addDX(2.0);
+
+        // verify
+        assert(v.getDX() == 8.3 + 2.0);
+
+        // teardown
+
+    }
+    void addDX2()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDX(-0.1);
+        v.addDX(-1.9);
+
+        // verify
+        assert(v.getDX() == -0.1 + -1.9);
+
+        // teardown
+
+    }
+    void addDY()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDY(8.3);
+        v.addDY(2.0);
+
+        // verify
+        assert(v.getDY() == 8.3 + 2.0);
+
+        // teardown
+
+    }
+    void addDY2()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDY(-7.2);
+        v.addDY(-2.8);
+
+        // verify
+        assert(v.getDY() == -2.8 + -7.2);
+
+        // teardown
+
+    }
+    void reverse()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDY(8.3);
+        v.addDY(2.0);
+
+        // verify
+        assert(v.getDY() == -8.3);
+        assert(v.getDX() == -2.0);
+        // teardown
+
+    }
+    void reverse2()
+    {  // setup
+        Velocity v;
+        // exercise
+
+        v.setDY(-8.3);
+        v.addDY(-2.0);
+
+        // verify
+        assert(v.getDY() == 8.3);
+        assert(v.getDX() == 2.0);
+        // teardown
+
+    }
+};
--- velocity.h
+++ velocity.h
@@ -1,1 +1,87 @@
 #pragma once
+/***********************************************************************
+ * Header File:
+ *    Velocity : Represents the velocity in the artillery simulation
+ * Author:
+ *    Garrett Badger
+ * Summary:
+ *    Everything about the velocity
+ ************************************************************************/
+
+#pragma once
+
+#include "position.h"   // for Point
+#include "uiDraw.h"
+#include "direction.h"
+
+ // forward declaration for the Velocity unit tests
+class TestVelocity;
+
+/***********************************************************
+ * Velocity
+ * The velocity class
+ ***********************************************************/
+class Velocity
+{
+public:
+	Velocity() { this->dx = 0; this->dy = 0; };
+	double getDX()
+	{
+		return this->dx;
+	}
+	double getDY()
+	{
+		return this->dy;
+	}
+	double getSpeed()
+	{
+		return speed;
+	}
+	Direction getDirection()
+	{
+		Direction direction;
+		return direction;
+	}
+	void setDX(double dx)
+	{
+		this->dx = dx;
+	}
+	void setDY(double dy)
+	{
+		this->dy = dy;
+	}
+	void setDxDy(double dx, double dy)
+	{
+		this->dx = dx;
+		this->dy = dy;
+	}
+	void setDirection(Direction direction)
+	{
+		
+	}
+	void setSpeed(double speed)
+	{
+		this->speed = speed;
+	}
+	void addDX(double dx)
+	{
+		this->dx += dx;
+	}
+	void addDY(double dy)
+	{
+		this->dy += dy;
+	}
+	void addV(Velocity velocity)
+	{
+
+	}
+	void reverse()
+	{
+		this->dx = -1 * dx;
+		this->dy = -1 * dy;
+	}
+private:
+	double dx;
+	double dy;
+	double speed;
+};
